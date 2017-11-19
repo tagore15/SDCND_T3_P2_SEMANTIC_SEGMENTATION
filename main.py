1f6c9bb28a6f4910a7f747aea7ad6525a3274f83
Atul Aggarwal <atul.unicode@gmail.com>
2017-11-19 23:56:28 +0530

    integerated review comments

--- main.py
+++ main.py
@@ -59,15 +59,19 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :return: The Tensor for the last layer of output
     """
     # TODO: Implement function
-    layer7_1x1 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
-    layer4_1x1 = tf.layers.conv2d(vgg_layer4_out, num_classes, 1, padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
-    layer3_1x1 = tf.layers.conv2d(vgg_layer3_out, num_classes, 1, padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    layer7_1x1 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
+                                kernel_initializer=tf.truncated_normal_initializer(stddev=0.01))
+    layer4_1x1 = tf.layers.conv2d(vgg_layer4_out, num_classes, 1, padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
+                                kernel_initializer=tf.truncated_normal_initializer(stddev=0.01))
+    layer3_1x1 = tf.layers.conv2d(vgg_layer3_out, num_classes, 1, padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
+                                kernel_initializer=tf.truncated_normal_initializer(stddev=0.01))
 
-    layer7_upsample = tf.layers.conv2d_transpose(layer7_1x1, num_classes, 4, 2, padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
-    layer4_upsample = tf.layers.conv2d_transpose(layer7_1x1, num_classes, 4, 2, padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    layer7_upsample = tf.layers.conv2d_transpose(layer7_1x1, num_classes, 4, 2, padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
+                                                kernel_initializer=tf.truncated_normal_initializer(stddev=0.01))
 
     layer4_skip = tf.add(layer7_upsample, layer4_1x1)
-    layer4_skip_up = tf.layers.conv2d_transpose(layer4_skip, num_classes, 4, 2, padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    layer4_skip_up = tf.layers.conv2d_transpose(layer4_skip, num_classes, 4, 2, padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
+                                                kernel_initializer=tf.truncated_normal_initializer(stddev=0.01))
     layer3_skip = tf.add(layer4_skip_up, layer3_1x1)
 
     output = tf.layers.conv2d_transpose(layer3_skip, num_classes, 16, 8, padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
@@ -165,3 +169,4 @@ def run():
 
 if __name__ == '__main__':
     run()
+
